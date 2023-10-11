729dc644eb933b131cb5a7a0a9c1f72fe0ffe028
dakkenkd <dakkenkd425@gmail.com>
2023-10-11 14:35:22 +0900

    update

---
+++ docs/precache-manifest.b65e2a0bc22d7e024825d514b4cff266.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4dec954b4cd7149a4e2baa36d64acb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c025b6919d47324a359",
+    "url": "./static/css/2.098d4aaa.chunk.css"
+  },
+  {
+    "revision": "5a26fc8f3c257dc0746a",
+    "url": "./static/css/main.2d3fad22.chunk.css"
+  },
+  {
+    "revision": "4c025b6919d47324a359",
+    "url": "./static/js/2.a8eb74d3.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "./static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "5a26fc8f3c257dc0746a",
+    "url": "./static/js/main.9bbd4245.chunk.js"
+  },
+  {
+    "revision": "a3a07417fc4be2fe8285",
+    "url": "./static/js/runtime~main.d0a23527.js"
+  },
+  {
+    "revision": "2f8c2669a8c02778e83ef9bcc5c16f7c",
+    "url": "./static/media/ASPR.2f8c2669.png"
+  },
+  {
+    "revision": "31be877b035646b7528f329ccb95bf28",
+    "url": "./static/media/UOH-DATABASE.31be877b.png"
+  }
+]);
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ImageGallery from "react-image-gallery";
-import "./Gallery.css"
+import "./Gallery.css";
+import img1 from "../image/ASPR.png";
+import img2 from "../image/UOH-DATABASE.png";
+
 
 export const Gallery = () => {
     const images = [
         {
-          original: 'https://picsum.photos/id/1018/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1018/250/150/',
+          original: img2,
+          thumbnail: img2,
         },
         {
-          original: 'https://picsum.photos/id/1015/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-          original: 'https://picsum.photos/id/1019/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1019/250/150/',
+          original: img1,
+          thumbnail: img1,
         },
       ];
 
